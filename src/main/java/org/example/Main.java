c0a92d10b2712705924f5b38a2f9f91a41331815
lwrb172 <lwrbielak@gmail.com>
2024-05-24 10:47:36 +0200

    z1 - adjustment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ImageProcessing imageProcessing = new ImageProcessing();
         try {
-            imageProcessing.read("thailand.jpg");
+            imageProcessing.read("thailand1.jpg");
             imageProcessing.write("thailand2.jpg");
         } catch (IOException e) {
             e.printStackTrace();
